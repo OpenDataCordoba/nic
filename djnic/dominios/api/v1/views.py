3c79c6242eb7103f61b62d98328d1c54ee2fd0c5
Andres Vazquez <andres@data99.com.ar>
2020-10-20 00:49:19 -0300

    Filter and order API

--- djnic/dominios/api/v1/views.py
+++ djnic/dominios/api/v1/views.py
@@ -1,8 +1,17 @@
 from rest_framework import viewsets
 from dominios.models import Dominio
 from .serializer import DominioSerializer
+from rest_framework.permissions import DjangoModelPermissions
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 
 class DominioViewSet(viewsets.ModelViewSet):
     queryset = Dominio.objects.all()
     serializer_class = DominioSerializer
+    permission_classes = [DjangoModelPermissions]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['estado', 'nombre', 'expire']
+    search_fields = ['nombre']
+    ordering_fields = '__all__'
+    ordering = ['nombre']
