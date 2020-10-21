12d69f12ccfee92c109432bb8a3a85d1de9ed3d5
Andres Vazquez <andres@data99.com.ar>
2020-10-21 20:43:57 -0300

    Serialize full domain

--- djnic/dominios/api/v1/serializer.py
+++ djnic/dominios/api/v1/serializer.py
@@ -2,10 +2,12 @@ from rest_framework import serializers
 from dominios.models import Dominio
 
 
-class DominioSerializer(serializers.HyperlinkedModelSerializer):
+class DominioSerializer(serializers.ModelSerializer):
+    domain = serializers.CharField(read_only=True, source='full_domain')
     class Meta:
         model = Dominio
-        fields = ['id', 'nombre', 'data_updated', 'data_readed', 'estado',
+        
+        fields = ['id', 'domain', 'data_updated', 'data_readed', 'estado',
                   'registered', 'changed', 'expire', 'priority_to_update',
                   'next_update_priority']    
                 # TODO add zona
