aefbfe1ff0fb06c6e6b39aad98b1eba34490be30
Andres Vazquez <andres@data99.com.ar>
2020-10-22 00:05:23 -0300

    preparing API post

--- djnic/dominios/api/v1/views.py
+++ djnic/dominios/api/v1/views.py
@@ -1,4 +1,6 @@
+from datetime import timedelta
 import random
+from django.utils import timezone
 from rest_framework import viewsets
 from dominios.models import Dominio
 from .serializer import DominioSerializer
@@ -6,6 +8,8 @@ from rest_framework.permissions import DjangoModelPermissions
 from rest_framework.authentication import TokenAuthentication
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from django.http import JsonResponse
+from rest_framework.decorators import action
 
 
 class DominioViewSet(viewsets.ModelViewSet):
@@ -19,14 +23,46 @@ class DominioViewSet(viewsets.ModelViewSet):
     ordering_fields = '__all__'
     ordering = ['nombre']
 
+    @action(methods=['post'])
+    def update_from_whoare(self, request):
+        data = request.data
+
+        if data.get('whoare_version', None) is None:
+            return JsonResponse({'ok': False, 'error': 'Missing WhoAre version'}, status_code=400)
+        
+        if data['whoare_version'] < '0.1.29':
+            return JsonResponse({'ok': False, 'error': 'Unexpected WhoAre version'}, status_code=400)
+        
+        "domain": {
+            "base_name": 'fernet',
+            "zone": 'com.ar',
+            "is_free": False,
+            "registered": wa.domain.registered,
+            "changed": wa.domain.changed,
+            "expire": wa.domain.expire
+            },
+        "registrant": {
+            "name": wa.registrant.name,
+            "legal_uid": wa.registrant.legal_uid,
+            "created": wa.registrant.created,
+            "changed": wa.registrant.changed
+        },
+        "dnss": ['ns2.sedoparking.com', 'ns1.sedoparking.com']
+
+
 
 class NextPriorityDomainViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         queryset = Dominio.objects.all().order_by('-priority_to_update')[:100]
         random_item = random.choice(queryset)
-        rand_id = random_item.id
-        return Dominio.objects.filter(pk=rand_id)
+        
+        # remove priority until tomorrow
+        random_item.priority_to_update = 0
+        random_item.next_update_priority = timezone.now() + timedelta(days=3)    
+        random_item.save()
+
+        return Dominio.objects.filter(pk=random_item.id)
     
     serializer_class = DominioSerializer
     
