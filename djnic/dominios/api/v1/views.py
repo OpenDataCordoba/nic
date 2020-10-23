67f2194f51c5fa7ce1a4b912f9442290933f84ef
Andres Vazquez <andres@data99.com.ar>
2020-10-22 23:06:24 -0300

    update API

--- djnic/dominios/api/v1/views.py
+++ djnic/dominios/api/v1/views.py
@@ -1,16 +1,21 @@
 from datetime import timedelta
+import json
+import logging
 import random
+from whoare.whoare import WhoAre
 from django.utils import timezone
 from rest_framework import viewsets
-from dominios.models import Dominio
-from .serializer import DominioSerializer
 from rest_framework.permissions import DjangoModelPermissions
 from rest_framework.authentication import TokenAuthentication
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from django.http import JsonResponse
 from rest_framework.decorators import action
+from dominios.models import Dominio
+from zonas.models import Zona
+from .serializer import DominioSerializer
 
+logger = logging.getLogger(__name__)
 
 class DominioViewSet(viewsets.ModelViewSet):
     queryset = Dominio.objects.all()
@@ -23,32 +28,39 @@ class DominioViewSet(viewsets.ModelViewSet):
     ordering_fields = '__all__'
     ordering = ['nombre']
 
-    @action(methods=['post'])
+    @action(methods=['post'], detail=False)
     def update_from_whoare(self, request):
-        data = request.data
-
-        if data.get('whoare_version', None) is None:
+        data = request.data  # require to be parsed
+        logger.info(f'update_from_whoare: {data}')
+        
+        real_data_str = data['domain']
+        logger.info(f'real data: {real_data_str}')
+        
+        # final_data = ast.literal_eval(real_data_str)
+        final_data = json.loads(real_data_str)
+        
+        if final_data.get('whoare_version', None) is None:
             return JsonResponse({'ok': False, 'error': 'Missing WhoAre version'}, status_code=400)
         
-        if data['whoare_version'] < '0.1.29':
+        if final_data['whoare_version'] < '0.1.29':
             return JsonResponse({'ok': False, 'error': 'Unexpected WhoAre version'}, status_code=400)
         
-        "domain": {
-            "base_name": 'fernet',
-            "zone": 'com.ar',
-            "is_free": False,
-            "registered": wa.domain.registered,
-            "changed": wa.domain.changed,
-            "expire": wa.domain.expire
-            },
-        "registrant": {
-            "name": wa.registrant.name,
-            "legal_uid": wa.registrant.legal_uid,
-            "created": wa.registrant.created,
-            "changed": wa.registrant.changed
-        },
-        "dnss": ['ns2.sedoparking.com', 'ns1.sedoparking.com']
-
+        wa = WhoAre()
+        wa.from_dict(final_data)
+        
+        zona, _ = Zona.objects.get_or_create(nombre=wa.domain.zone)
+        dominio, dominio_created = Dominio.objects.get_or_create(
+            nombre=wa.domain.base_name,
+            zona=zona
+            )
+        
+        cambios = dominio.update_from_wa_object(wa, just_created=dominio_created)
+        res = {
+            'ok': True,
+            'created': dominio_created,
+            'cambios': cambios
+        }
+        return JsonResponse(res)
 
 
 class NextPriorityDomainViewSet(viewsets.ModelViewSet):
@@ -57,7 +69,7 @@ class NextPriorityDomainViewSet(viewsets.ModelViewSet):
         queryset = Dominio.objects.all().order_by('-priority_to_update')[:100]
         random_item = random.choice(queryset)
         
-        # remove priority until tomorrow
+        # remove priority
         random_item.priority_to_update = 0
         random_item.next_update_priority = timezone.now() + timedelta(days=3)    
         random_item.save()
