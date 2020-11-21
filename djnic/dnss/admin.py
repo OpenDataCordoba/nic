02ac6dff1639888578a4bfe673aee175f1724574
Andres Vazquez <andres@data99.com.ar>
2020-11-21 10:48:15 -0300

    Start regex for DNS

--- djnic/dnss/admin.py
+++ djnic/dnss/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
-from .models import DNS
+from .models import DNS, Empresa, EmpresaRegexDomain
+
 
 @admin.register(DNS)
 class DNSAdmin(admin.ModelAdmin):
-    list_display = ['dominio']
+    list_display = ['dominio', 'empresa_regex']
     list_per_page = 10
     search_fields = ['dominio']
+
+
+@admin.register(Empresa)
+class EmpresaAdmin(admin.ModelAdmin):
+    list_display = ['nombre']
+    list_per_page = 10
+    search_fields = ['nombre']
+
+
+@admin.register(EmpresaRegexDomain)
+class EmpresaRegexDomainAdmin(admin.ModelAdmin):
+    list_display = ['empresa', 'regex_dns']
+    list_per_page = 10
+    search_fields = ['empresa__nombre', 'regex_dns']
--- djnic/dnss/models.py
+++ djnic/dnss/models.py
@@ -1,19 +1,52 @@
+import logging
+import re
 from django.db import models
 
+logger = logging.getLogger(__name__)
 
 class Empresa(models.Model):
     """ una empresa de hosting """
     nombre = models.CharField(max_length=90)
+    
+    def __str__(self):
+        return self.nombre
+
+    def detect_DNSs(self):
+        """ returns a list of querysets with all DNSs """
+        ret = []
+        for rg in self.regexs:
+            ret.append(DNS.objects.filter(dominio__regex=rg.regex_dns))
+        return ret
+
+    def connect_all(self):
+        """ conectar esta empresa a todos los DNSs detectados """
+        querysets = self.detect_DNSs()
+        for qs in querysets:
+            qs.update(empresa=self)
+
+
+class EmpresaRegexDomain(models.Model):
+    """ cada una de las expresiones regulares para detectar dominios que le pertenecen """
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE, related_name='regexs')
     regex_dns = models.CharField(max_length=190, help_text='Experesion regular para encontrar los DNSs que le pertenecen', null=True, blank=True)
+
+    def __str__(self):
+        returf f'{self.empresa} {self.regex_dns}'
 
 
 class DNS(models.Model):
-    empresa = models.ForeignKey(Empresa, null=True, blank=True, on_delete=models.SET_NULL, related_name='nameservers')
+    empresa_regex = models.ForeignKey(EmpresaRegexDomain, null=True, blank=True, on_delete=models.SET_NULL, related_name='nameservers')
     dominio = models.CharField(max_length=240, unique=True)
 
+    def __str__(self):
+        return self.dominio
+
     def assign_empresa(self):
-        # TODO buscar empresa con regex
-        pass
+        for rgs in EmpresaRegexDomain.objects.all():
+            if re.search(rgs.regex_dns ,self.dominio) is not None:
+                self.empresa_regex = rgs
+                return
+        logger.error(f'No regex found for {self.dominio}')
     
     def save(self, **kwargs):
         # buscar la empresa a la que pertenece
