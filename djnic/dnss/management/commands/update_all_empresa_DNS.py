ef7e3d67cd5d97371496ff2d4cca26439817f24a
Andres Vazquez <andres@data99.com.ar>
2020-11-28 10:30:32 -0300

    Update all DNS command

---
+++ djnic/dnss/management/commands/update_all_empresa_DNS.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Count, Q
+from dnss.models import Empresa
+
+
+class Command(BaseCommand):
+    help = 'Buscar DNSs que no pertenezcan a una empresa'
+
+    def handle(self, *args, **options):
+
+        self.stdout.write(self.style.SUCCESS("Buscando Empresas"))
+        
+        for empresa in Empresa.objects.all():
+            self.stdout.write(self.style.SUCCESS(f'{empresa}'))
+            empresa.detect_DNSs()
+            
+        self.stdout.write(self.style.SUCCESS("FIN"))
