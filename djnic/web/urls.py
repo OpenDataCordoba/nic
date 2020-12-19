23234ec743ee91dab8aa29b24b48c7145ce5c65c
Andres Vazquez <andres@data99.com.ar>
2020-12-19 20:45:00 -0300

    Home dominios

--- djnic/web/urls.py
+++ djnic/web/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from .views import HomeView
+from .views_dominio import DominioView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('dominio-<int:pk>', DominioView.as_view(), name='dominio'),
 ]
---
+++ djnic/web/views_dominio.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page, cache_control
+from django.views.generic.detail import DetailView
+
+from dominios.models import Dominio, STATUS_DISPONIBLE
+
+
+@method_decorator(cache_control(max_age=settings.GENERAL_CACHE_SECONDS), name='dispatch')
+@method_decorator(cache_page(settings.GENERAL_CACHE_SECONDS), name='dispatch')
+class DominioView(DetailView):
+
+    model = Dominio
+    context_object_name = "dominio"
+    template_name = "web/bootstrap-base/dominio.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['site_title'] = f'Dominio {self.object.full_domain()}'
+        context['site_description'] = f'Datos del Dominio {self.object.full_domain()}'
+        context['estado'] = 'Disponible' if self.object.estado == STATUS_DISPONIBLE else 'No disponible'
+
+        return context
