c74d5ec15ed95dacb7db6fd54f4a762c26ae28c8
Andres Vazquez <andres@data99.com.ar>
2020-12-29 20:25:49 -0300

    Cache sitemap

--- djnic/djnic/urls.py
+++ djnic/djnic/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
+from django.views.decorators.cache import cache_page
 from rest_framework.authtoken import views
 
 from .views import DominioSitemap, RegistranteSitemap, RubroSitemap, HostingSitemap
@@ -21,5 +22,5 @@ urlpatterns = [
     path('api/', include('djnic.api.urls')),
     path('api-token-auth/', views.obtain_auth_token),
     path('', include('web.urls')),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
+    path('sitemap.xml', cache_page(86400)(sitemap), {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
