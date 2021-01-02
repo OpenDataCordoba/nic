701001c163f5695baa20db4b5a860bf5c07f83ae
Andres Vazquez <andres@data99.com.ar>
2021-01-02 01:36:49 -0300

    Force update

--- djnic/web/static/web/plataforma/js/reading_general_chart.js
+++ djnic/web/static/web/plataforma/js/reading_general_chart.js
@@ -13,7 +13,7 @@ function drawChart1() {
     var chartDiv3 = document.getElementById('chart_div3');
 
     var jsonData = $.ajax({
-        url: "/api/v1/dominios/stats/general",
+        url: "/api/v1/dominios/stats/general?p=1",
         dataType: "json",
         async: false
         });
