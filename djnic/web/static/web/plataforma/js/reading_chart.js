e9d0a0d37b0335472ab661e24f9f45c06eece8ab
Andres Vazquez <andres@data99.com.ar>
2021-01-03 09:54:04 -0300

    more chart

--- djnic/web/static/web/plataforma/js/reading_chart.js
+++ djnic/web/static/web/plataforma/js/reading_chart.js
@@ -37,7 +37,7 @@ function drawChart2() {
     var chartDiv2 = document.getElementById('chart_div2');
 
     var jsonData = $.ajax({
-        url: "/api/v1/dominios/stats/reading/-300/420",
+        url: "/api/v1/dominios/stats/reading/-45/300",
         dataType: "json",
         async: false
         });
@@ -54,5 +54,30 @@ function drawChart2() {
         }
 
     var materialChart = new google.charts.Bar(chartDiv2);
+    materialChart.draw(data, google.charts.Bar.convertOptions(materialOptions));
+}
+
+function drawChart3() {
+
+    var chartDiv3 = document.getElementById('chart_div3');
+
+    var jsonData = $.ajax({
+        url: "/api/v1/dominios/stats/reading/-300/420",
+        dataType: "json",
+        async: false
+        });
+
+    var data = google.visualization.arrayToDataTable(jsonData.responseJSON.data.google_chart_data);
+
+    var materialOptions = {
+        isStacked: true,
+        chart: {
+            title: 'Lectura de dominios',
+            subtitle: 'Dias desde ultima lectura por dia de vencimiento de dominio',
+            legend: { position: "none" },
+            },
+        }
+
+    var materialChart = new google.charts.Bar(chartDiv3);
     materialChart.draw(data, google.charts.Bar.convertOptions(materialOptions));
 }
