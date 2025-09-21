6a02ed50519fcd2c852e20e2b992585e593ee230
avdata99 <andres@data99.com.ar>
2025-09-21 12:42:47 -0300

    track all

--- djnic/core/views.py
+++ djnic/core/views.py
@@ -5,11 +5,13 @@ class AnalyticsViewMixin:
     def get(self, request, *args, **kwargs):
         if not request.user.is_anonymous:
             user = request.user
-            path = request.path.strip('/').split('/')[-1:][0]
-            Analytic.objects.create(
-                evento='view',
-                user=user,
-                referencia=path,
-                extras=Analytic.request_as_dict(request)
-            )
+        else:
+            user = None
+        path = request.path.strip('/').split('/')[-1:][0]
+        Analytic.objects.create(
+            evento='view',
+            user=user,
+            referencia=path,
+            extras=Analytic.request_as_dict(request)
+        )
         return super().get(request, *args, **kwargs)
