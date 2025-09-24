7db52d5fda9372506c2631a798080ff10533ffe8
avdata99 <andres@data99.com.ar>
2025-09-24 13:49:51 -0300

    Older first

--- djnic/dominios/api/v1/views.py
+++ djnic/dominios/api/v1/views.py
@@ -13,16 +13,17 @@ from rest_framework import filters
 from django.http import JsonResponse
 from django.db.models import F, Q
 from django.utils.decorators import method_decorator
-from django.views.decorators.cache import never_cache, cache_page, cache_control
+from django.views.decorators.cache import never_cache, cache_page
 from rest_framework.decorators import action
 from dominios.models import Dominio, STATUS_DISPONIBLE, STATUS_NO_DISPONIBLE, PreDominio
 from zonas.models import Zona
 from cambios.models import CampoCambio
-from .serializer import (DominioSerializer, CambiosDominioSerializer, 
+from .serializer import (DominioSerializer, CambiosDominioSerializer,
                          FlatDominioSerializer, FlatPreDominioSerializer,
                          PreDominioSerializer)
 
 logger = logging.getLogger(__name__)
+
 
 class DominioViewSet(viewsets.ModelViewSet):
     queryset = Dominio.objects.all()
@@ -39,22 +40,22 @@ class DominioViewSet(viewsets.ModelViewSet):
     def update_from_whoare(self, request):
         data = request.data  # require to be parsed
         logger.info(f'update_from_whoare: {data}')
-        
+
         real_data_str = data['domain']
         logger.info(f'real data: {real_data_str}')
-        
+
         # final_data = ast.literal_eval(real_data_str)
         final_data = json.loads(real_data_str)
-        
+
         if final_data.get('whoare_version', None) is None:
             return JsonResponse({'ok': False, 'error': 'Missing WhoAre version'}, status=400)
-        
+
         if final_data['whoare_version'] < '0.1.40':
             return JsonResponse({'ok': False, 'error': 'Unexpected WhoAre version'}, status=400)
-        
+
         wa = WhoAre()
         wa.from_dict(final_data)
-        
+
         # if exists at pre-domains, remove it: we are done with this domain
         pres = PreDominio.objects.filter(dominio=wa.domain.full_name())
         if pres.count() > 0:
@@ -63,19 +64,18 @@ class DominioViewSet(viewsets.ModelViewSet):
 
         # skipp not-real domains when it come from pre-domains
         if wa.domain.is_free:
-            
+
             # Si ya existe entonces es un update, si no entonces no lo necesitamos
             dominio = Dominio.get_from_full_domain(wa.domain.full_name())
             if dominio is None:
                 return JsonResponse({'ok': False, 'error': 'We expect a REGISTERED domain'}, status=400)
 
-        
         zona, _ = Zona.objects.get_or_create(nombre=wa.domain.zone)
         dominio, dominio_created = Dominio.objects.get_or_create(
             nombre=wa.domain.base_name,
             zona=zona
             )
-        
+
         cambios = dominio.update_from_wa_object(wa, just_created=dominio_created)
         res = {
             'ok': True,
@@ -97,7 +97,7 @@ class PreDominioViewSet(viewsets.ModelViewSet):
 
 @method_decorator(never_cache, name='dispatch')
 class NextPriorityDomainViewSet(viewsets.ModelViewSet):
-    
+
     permission_classes = [DjangoModelPermissions]
     authentication_classes = [TokenAuthentication, SessionAuthentication]
 
@@ -111,11 +111,11 @@ class NextPriorityDomainViewSet(viewsets.ModelViewSet):
         else:
             res = self.get_from_predomain()
             return res
-    
+
     def get_from_domain(self):
         prioritarios = Dominio.objects.all().order_by('-priority_to_update')[:100]
         random_item = random.choice(prioritarios)
-        
+
         # remove priority
         random_item.priority_to_update = 0
         random_item.next_update_priority = timezone.now() + timedelta(days=5)
@@ -123,19 +123,19 @@ class NextPriorityDomainViewSet(viewsets.ModelViewSet):
         self.serializer_class = FlatDominioSerializer
         res = Dominio.objects.filter(pk=random_item.id)
         return res
-    
+
     def get_from_predomain(self):
-        
+
         nuevos = PreDominio.objects.filter(priority__gt=0)
-        nuevos = nuevos.order_by('-priority', 'dominio')[:100]
+        nuevos = nuevos.order_by('-priority', 'object_created')[:100]
         random_item = random.choice(nuevos)
         if random_item.priority == 0:
             # se acabaron
             return self.get_from_domain()
-        
+
         random_item.priority = 0
         random_item.save()
-        
+
         # si ya existe en dominios, omitir
         wa = WhoAre()
         domain_name, zone = wa.detect_zone(random_item.dominio)
