44b16b7615c41ec8bc7c4dcd4a275cc3fcc3ee00
avdata99 <andres@data99.com.ar>
2025-09-28 16:54:12 -0300

    prepare to log runs

--- djnic/dominios/management/commands/update_priority.py
+++ djnic/dominios/management/commands/update_priority.py
@@ -1,4 +1,5 @@
 import logging
+import time
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 from dominios.models import Dominio
@@ -37,6 +38,9 @@ class Command(BaseCommand):
                     f"{old_ptu} => {dominio.priority_to_update}"
                 )
             )
+        # sleep every 1000
+        if c and c % 1000 == 0:
+            time.sleep(4)
 
         report = f"{c} processed"
         self.stdout.write(self.style.SUCCESS(report))
