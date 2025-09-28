05bbb3348c0fb6d0dbb088d38d7f7ff4bd337ad8
avdata99 <andres@data99.com.ar>
2025-09-28 15:56:56 -0300

    improve domain admin

--- djnic/dominios/admin.py
+++ djnic/dominios/admin.py
@@ -1,17 +1,23 @@
 from django.contrib import admin
 from .models import Dominio, PreDominio
 
+
 @admin.register(Dominio)
 class DominioAdmin(admin.ModelAdmin):
 
     def nameservers(self, obj):
         return ' '.join([f'{dns.orden}: {dns.dns.dominio}' for dns in obj.dnss.order_by('orden')])
 
-    list_display = ['nombre', 'zona', 'uid', 'estado', 'data_updated', 'data_readed', 'registrante', 'registered', 'changed', 'expire', 'priority_to_update', 'nameservers']
+    list_display = [
+        'nombre', 'zona', 'estado', 'priority_to_update', 'next_update_priority', 'data_updated', 'data_readed',
+        'registrante', 'registered', 'changed', 'expire', 'nameservers'
+    ]
     list_per_page = 10
     list_select_related = ('zona', 'registrante')
     search_fields = ['nombre']
     list_filter = ['estado', 'zona']
+    # remove huge selectors for the change view (like registrante)
+    readonly_fields = ['registrante']
 
 
 @admin.register(PreDominio)
