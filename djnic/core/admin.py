a38beb4b3554732f993e002354ae3cf23df369e1
avdata99 <andres@data99.com.ar>
2025-09-29 18:23:21 -0300

    Page size

--- djnic/core/admin.py
+++ djnic/core/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from core.models import News
 
+
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
     list_display = ['title', 'priority', 'object_created', 'description']
 
+    def changelist_view(self, request, extra_context=None):
+        page_size = request.GET.get('page_size')
+        if page_size and page_size.isdigit():
+            self.list_per_page = int(page_size)
+        else:
+            self.list_per_page = 20
+        return super().changelist_view(request, extra_context)
--- djnic/dominios/admin.py
+++ djnic/dominios/admin.py
@@ -8,17 +8,18 @@ class DominioAdmin(admin.ModelAdmin):
     def nameservers(self, obj):
         return ' '.join([f'{dns.orden}: {dns.dns.dominio}' for dns in obj.dnss.order_by('orden')])
 
-    def get_list_per_page(self, request):
+    def changelist_view(self, request, extra_context=None):
         page_size = request.GET.get('page_size')
         if page_size and page_size.isdigit():
-            return int(page_size)
-        return self.list_per_page
+            self.list_per_page = int(page_size)
+        else:
+            self.list_per_page = 20
+        return super().changelist_view(request, extra_context)
 
     list_display = [
         'nombre', 'zona', 'estado', 'priority_to_update', 'next_update_priority', 'data_updated', 'data_readed',
         'registrante', 'registered', 'changed', 'expire', 'nameservers'
     ]
-    list_per_page = 50
     list_select_related = ('zona', 'registrante')
     search_fields = ['nombre']
     list_filter = ['estado', 'zona']
@@ -29,13 +30,14 @@ class DominioAdmin(admin.ModelAdmin):
 @admin.register(PreDominio)
 class PreDominioAdmin(admin.ModelAdmin):
 
-    def get_list_per_page(self, request):
+    def changelist_view(self, request, extra_context=None):
         page_size = request.GET.get('page_size')
         if page_size and page_size.isdigit():
-            return int(page_size)
-        return self.list_per_page
+            self.list_per_page = int(page_size)
+        else:
+            self.list_per_page = 20
+        return super().changelist_view(request, extra_context)
 
     list_display = ['dominio', 'priority', 'object_created']
-    list_per_page = 50
     search_fields = ['dominio']
     list_filter = ['priority']
