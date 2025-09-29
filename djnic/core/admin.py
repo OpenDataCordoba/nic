bf632be3afac3997912ce742ed991b0f38c1c9cc
avdata99 <andres@data99.com.ar>
2025-09-29 18:46:30 -0300

    no

--- djnic/core/admin.py
+++ djnic/core/admin.py
@@ -7,7 +7,7 @@ class NewsAdmin(admin.ModelAdmin):
     list_display = ['title', 'priority', 'object_created', 'description']
 
     def get_changelist_instance(self, request):
-        page_size = request.GET.pop('page-size', 20)
+        page_size = request.GET.get('page-size', 20)
         if page_size:
             self.list_per_page = int(page_size)
         return super().get_changelist_instance(request)
