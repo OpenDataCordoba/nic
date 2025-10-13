034b0a1ff94d20a4f30899ad5c4d4f2c04cf4b6d
avdata99 <andres@data99.com.ar>
2025-10-13 11:02:23 -0300

    Change priority Trying to improve

--- djnic/dominios/models.py
+++ djnic/dominios/models.py
@@ -308,13 +308,10 @@ class Dominio(models.Model):
             raise Exception('Unknown domain')
 
         self.priority_to_update = priority
-        if priority == 0:
-            self.next_update_priority = timezone.now() + timedelta(days=180 + randint(0, 30))
-        elif priority < 2_000_000:
-            days = 40 - int(priority / 100_000)
-            self.next_update_priority = timezone.now() + timedelta(days=days)
+        if priority > 5_000_000:
+            self.next_update_priority = timezone.now() + timedelta(days=5)
         else:
-            self.next_update_priority = timezone.now() + timedelta(days=3)
+            self.next_update_priority = timezone.now() + timedelta(days=15)
 
         data = {
             'last_priority_calc': {
--- djnic/dominios/priority/ar.py
+++ djnic/dominios/priority/ar.py
@@ -13,30 +13,28 @@ def calculate_priority(expire_days, readed_days, updated_days, estado):
     priority = 0
     # si ya lo leí hace poco, al fondo
     if estado == STATUS_NO_DISPONIBLE:
-        if readed_days > 6:
-            # en Argentina los dominios caen 45 dias despues de vencidos
-            if expire_days >= 45 and expire_days < 95:
-                priority = 10_000_000 + (expire_days*10) + (readed_days*5) + updated_days
-            elif expire_days > 94 and expire_days < 365:
-                # Demoras nuestras probablemente
-                priority = 7_000_000 + (expire_days*5) + (readed_days*5) + updated_days
-            elif expire_days > 364 and expire_days < 1530:
-                # Demoras nuestras probablemente
-                priority = 5_000_000 + (expire_days*2) + (readed_days*5) + updated_days
-            elif expire_days > 1529:
-                # probablemente judicializados y cosas sin sentido
-                priority = 3_000_000 + (expire_days*2) + (readed_days*5) + updated_days
-            elif expire_days >= -25 and expire_days < 46:
-                priority = 1_000_000 + (expire_days*2) + (readed_days*5) + updated_days
-            elif expire_days < -25:
-                priority = 10_000 + expire_days + readed_days + updated_days
-            else:
-                # non expected, a gap in the selecion
-                priority = -2
+
+        # en Argentina los dominios caen 45 dias despues de vencidos
+        if expire_days >= 45 and expire_days < 95:
+            priority = 10_000_000 + (expire_days*10) + (readed_days*5) + updated_days
+        elif expire_days > 94 and expire_days < 365:
+            # Demoras nuestras probablemente
+            priority = 7_000_000 + (expire_days*5) + (readed_days*5) + updated_days
+        elif expire_days > 364 and expire_days < 1530:
+            # Demoras nuestras probablemente
+            priority = 5_000_000 + (expire_days*2) + (readed_days*5) + updated_days
+        elif expire_days > 1529:
+            # probablemente judicializados y cosas sin sentido
+            priority = 3_000_000 + (expire_days*2) + (readed_days*5) + updated_days
+        elif expire_days >= -25 and expire_days < 46:
+            priority = 1_000_000 + (expire_days*2) + (readed_days*5) + updated_days
+        elif expire_days < -25:
+            priority = 10_000 + expire_days + readed_days + updated_days
         else:
-            priority = readed_days * 3
+            # non expected, a gap in the selecion
+            priority = -2
+
     else:
-        if updated_days < 45:
-            priority = readed_days + updated_days
+        priority = (readed_days*10) + updated_days
 
     return priority
