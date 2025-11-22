dc6d6710ae6bbd4d88841180d9438afabad8bb0b
avdata99 <andres@data99.com.ar>
2025-11-22 20:20:55 -0300

    Update to Django 4.2

---
+++ djnic/djnic/test_settings.py
@@ -0,0 +1,26 @@
+from .settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': [],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10000/day',
+        'user': '100000/day'
+    },
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
+}
+
+TEST_RUNNER = 'djnic.test_runner.CustomTestRunner'
