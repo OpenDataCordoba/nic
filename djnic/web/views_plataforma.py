2e074bd84969d126791ed64e33409dae88b85562
avdata99 <andres@data99.com.ar>
2025-11-27 10:55:06 -0300

    Fix text

--- djnic/web/views_plataforma.py
+++ djnic/web/views_plataforma.py
@@ -10,7 +10,7 @@ class StatsReadVtoView(AnalyticsViewMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['site_title'] = 'Estadisticas de lecturas por vencimiento'
-        context['site_description'] = 'Dias desde la ultima lectura de domimios según fecha de vencimieeto del mismo'
+        context['site_description'] = 'Dias desde la ultima lectura de domimios según fecha de vencimiento del mismo'
 
         return context
 
