6414dabb81fe255e3a29ac2a7a113085d49df4f0
avdata99 <andres@data99.com.ar>
2025-12-02 14:18:20 -0300

    more results

--- djnic/core/data.py
+++ djnic/core/data.py
@@ -9,11 +9,11 @@ from django.conf import settings
 @cache_memoize(settings.GENERAL_CACHE_SECONDS)
 def get_search_results(query):
     res = {}
-    res['dominios'] = Dominio.objects.filter(nombre__icontains=query).order_by('nombre')[:50]
+    res['dominios'] = Dominio.objects.filter(nombre__icontains=query).order_by('nombre')[:250]
     res['registrantes'] = Registrante.objects.filter(
         Q(name__icontains=query) | Q(legal_uid__icontains=query)
-    ).order_by('name')[:50]
-    res['hostings'] = Empresa.objects.filter(nombre__icontains=query).order_by('nombre')[:50]
-    res['dnss'] = DNS.objects.filter(dominio__icontains=query).order_by('dominio')[:50]
+    ).order_by('name')[:250]
+    res['hostings'] = Empresa.objects.filter(nombre__icontains=query).order_by('nombre')[:250]
+    res['dnss'] = DNS.objects.filter(dominio__icontains=query).order_by('dominio')[:250]
 
     return res
